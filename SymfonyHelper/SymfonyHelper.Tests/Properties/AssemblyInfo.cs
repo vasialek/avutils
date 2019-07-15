a9121630df9c975f2b620008033434c4e691a0dd
Aleksej Vasinov <proglamer@gmail.com>
2019-07-15 14:32:32 +0300

    Added unit tests project for SymfonyHelper.

---
+++ SymfonyHelper/SymfonyHelper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SymfonyHelper.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SymfonyHelper.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4dde4d2-0202-4558-b0ea-154fc07d6407")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SymfonyHelper/SymfonyHelper.Tests/SqlParserTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SymfonyHelper.Definition.Interfaces;
+
+namespace SymfonyHelper.Tests
+{
+	[TestClass]
+	public class SqlParserTest
+	{
+		private readonly ISqlParser _parser = null;
+
+
+		public SqlParserTest()
+		{
+		}
+
+		#region Parse inserts into files table
+
+		[TestMethod]
+		public void ParseInsertIntoFiles_CheckCount()
+		{
+
+		}
+
+		#endregion
+	}
+}
