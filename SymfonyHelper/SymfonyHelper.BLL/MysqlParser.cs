55d58342987ec1ec12c5259d24717d0fa66e1c22
Aleksej Vasinov <proglamer@gmail.com>
2019-07-15 15:02:13 +0300

    Added BLL project for SymfonyHelper.

---
+++ SymfonyHelper/SymfonyHelper.BLL/MysqlParser.cs
@@ -0,0 +1,19 @@
+﻿using SymfonyHelper.Definition.Interfaces;
+using SymfonyHelper.Definition.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymfonyHelper.BLL
+{
+	public class MysqlParser : ISqlParser
+	{
+		public FilesModel[] ParseInsertIntoFiles(Stream stream)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
